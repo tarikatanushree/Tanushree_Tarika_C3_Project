dc9c989673cb0c096ebb6145dd803a701b4d18c7
tarikatanushree <tarikatanushree@gmail.com>
2023-08-28 20:26:37 +0530

    Part2:Solution Updated

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 public class RestaurantService {
     private static final List<Restaurant> restaurants = new ArrayList<>();
-
-    RestaurantService() {
-        LocalTime openingTime = LocalTime.parse("09:00:00");
-        LocalTime closingTime = LocalTime.parse("23:00:00");
-        restaurants.add(new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime));
-    }
+    
 
     public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
         //DELETE ABOVE STATEMENT AND WRITE CODE HERE
